eabdf0046aac205a98781b26e7f7e889494c89cd
charangowda <csn@indreamer.com>
2024-09-23 16:18:16 +0530

    Git Action is added

---
+++ src/app/jiffybook/BusinessSchema/page.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+
+
+import "./style.css";
+
+const BusinessSchema = (props) => {
+  return (
+    <div className="gallery-schema-container">
+     
+      <div className="gallery-schema-gallery-schema">
+        <div className="gallery-schema-header-new">
+          <img
+            src="/image/ellipse-1@2x.png"
+            alt="circlelogowithspace2I785"
+            className="gallery-schema-circlelogowithspace2"
+          />
+          <div className="gallery-schema-keyword14bo1">
+            <span className="gallery-schema-text10">
+              <span>CREATE A GALLERY</span>
+            </span>
+          </div>
+          <div className="gallery-schema-keyword14bo2">
+            <span className="gallery-schema-text12">
+              <span>Exit</span>
+            </span>
+          </div>
+        </div>
+        <div className="gallery-schema-keyword24bo">
+          <span className="gallery-schema-text14">
+            <span>Schema</span>
+          </span>
+        </div>
+        <div className="gallery-schema-frame63">
+          <div className="gallery-schema-frame41">
+            <div className="gallery-schema-keyword14bb1">
+              <span className="gallery-schema-text16">
+                <span>Select a pre-built schema</span>
+              </span>
+            </div>
+          </div>
+          <div className="gallery-schema-frame50">
+            <div className="gallery-schema-keyword14bb2">
+              <span className="gallery-schema-text18">
+                <span>Create a custom schema</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BusinessSchema
+
+
+
